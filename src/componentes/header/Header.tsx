453d3232a76071ef1f69b26219f4c53e61afe836
LUANA PIRES DELEVATTI <07364382119@ifms.edu.br>
2023-08-15 07:33:48 -0400

    primeiro commit

---
+++ src/componentes/header/Header.tsx
@@ -0,0 +1,20 @@
+import './Header.css'
+function Header(){
+    return(
+        <header>
+           <div>
+            Logo
+           </div>
+           <nav>
+           <ul>
+            <li><a href='#'>Home</a></li>
+            <li><a href='#'>Sobre</a></li>
+            <li><a href='#'>Noticia</a></li>
+            <li><a href='#'>Contato</a></li>
+           </ul>
+           </nav>
+        </header>
+
+    )
+}
+export default Header
